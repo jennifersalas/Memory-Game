9d41109546db128e891bb523b9dc4f5d90f1bc70
Salas, Jennifer (jq859t) <jq859t@att.com>
2019-07-31 17:19:23 -0700

    Flipping and matching done. Star decrementation, timer, and end game pop up needs to be implemented

--- js/app.js
+++ js/app.js
@@ -3,6 +3,19 @@
  */
 
 
+ let oneTurned = false;
+ let lastTurned = null;
+
+ const deck = document.querySelector('.deck');
+ const cards = document.querySelectorAll('.card');
+ const stars = document.querySelector('.stars');
+ const movesElem = document.querySelector('.moves');
+ let moves = 0;
+
+ let openCards = [];
+
+ document.querySelector('.restart').addEventListener('click', reset);
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -21,8 +34,62 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
+}
+
+function reset() {
+  startTurn = false;
+  moves = 0;
+  stars.innerHTML = "";
+  movesElem.innerHTML = moves.toString();
+  shuffled = shuffle(Array.from(cards));
+  shuffled.forEach(function(card) {
+    card.remove();
+    card.className = "card";
+    // card.classList.add('show', 'open');
+    setTimeout(0,deck.appendChild(card));
+  });
+}
+
+deck.addEventListener('click', function(event) {
+    if (event.target.nodeName === "LI") {
+      faceUp(event.target);
+    }
+    else if (event.target.nodeName == "I") {
+      faceUp(event.target.parentNode);
+    }
+});
+
+
+function faceUp(card) {
+  for(cl of card.classList) {
+    if (['open', 'show', 'match'].includes(cl)) return;
+  }
+  openCards.push(card);
+  if (openCards.length > 2) {
+    openCards.pop();
+  } else if (openCards.length == 2) {
+    card.classList.add('open', 'show');
+    setTimeout(endTurn, 500)
+  } else {
+    card.classList.add('open', 'show');
+  }
+}
+
+function endTurn() {
+  if (openCards[0].firstElementChild.className === openCards[1].firstElementChild.className) {
+    openCards.forEach(function(card) {
+      card.classList.add('match');
+    });
+  }
+  else {
+    openCards.forEach(function(card) {
+      card.classList.remove('open', 'show');
+    });
+  }
+  movesElem.innerHTML = ++moves;
+  if (moves % 5 == 0) stars.lastChild.remove();
+  openCards = [];
 }
 
 
